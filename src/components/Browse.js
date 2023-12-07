c96d61c8a52de75c38b87e456d69ca12ef0cbaea
pranaliahirrao <pranali.21810302@viit.ac.in>
2023-12-07 10:13:26 +0530

    teting

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -9,6 +9,7 @@ import useUpcomingMovies from "../hooks/useUpcomingMovies";
 import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
 
+
 const Browse = () => {
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
 
